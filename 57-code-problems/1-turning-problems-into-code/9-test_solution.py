e8ea1b90221b66a7f1630654ed86aa1d4ed10d6d
LondonComputadores <london.computadores@gmail.com>
2021-04-18 09:20:52 -0400

    REFACTORING JS VANILLA CODE: Just addind up a different way of writing the same programm that is an imc calculator

---
+++ 57-code-problems/1-turning-problems-into-code/9-test_solution.py
@@ -0,0 +1,34 @@
+"""
+    ToDo: 
+
+    - Create a simple tip calculator. The program should prompt
+    for a bill amount and a tip rate input. The program must calculate
+    the tip and then print both the tip and the total amount of
+    the bill
+    
+    Requirements:
+
+    - bill amount
+    - tip rate
+    - tip
+    - total amount
+
+    Parameters:
+
+    :param input_bill_amount
+    :type input_bill_amount: float
+    :param input_tip_rate
+    :type input_tip_rate: int
+    :param calculate_tip
+    :type calculate_tip: float
+    :param calculate_total_bill
+    :type calculate_total_bill: float
+    :param print(calculate_tip)
+    :param print(calculate_total_bill)
+
+"""
+
+import unittest
+
+
+class Test
--- js-all/randoms/imc_calculator/imc_calc.js
+++ js-all/randoms/imc_calculator/imc_calc.js
@@ -4,12 +4,11 @@
 // alert(greet)
 
 
+ 
+peso = prompt("Digite seu peso: ");
+altura = prompt("Digite sua altura: ");
 
-
-
-let peso = prompt("Digite seu peso: ");
-let altura = prompt("Digite sua altura: ");
-let imc = peso / (altura * altura);
+imc = peso / (altura * altura);
 
 alert("Seu resultado é de: " + imc);
 
@@ -38,3 +37,33 @@ else {
         }
     }
 }
+
+
+// Vamos Refazer Nosso Teste do IMC
+// Agora, com o uso de “AND”, podemos reescrever nosso código de cálculo de
+// IMC. É questionável se o programa resultante será melhor que o apresentado
+// originalmente, mas nosso objetivo aqui é didático.
+
+    
+// function imc() {  
+//   var peso = prompt("entre com seu peso");
+//   var altura = prompt("entre com sua altura");
+//   var imc = peso / (altura * altura);
+//   alert("Seu IMC ficou em: " + imc );
+//   if (imc < 17) alert("muito abaixo do peso");
+//   if ( (imc >= 17) && (imc < 18.5) ) alert("abaixo do peso");
+//   if ( (imc >= 18.5) && (imc < 25) ) alert("peso normal");
+//   if ( (imc >= 25) && (imc < 30) ) alert("acima do peso");
+//   if ( (imc >= 30) && (imc < 35) ) alert("obesidade I");
+//   if ( (imc >= 35) && (imc < 40) ) alert("obesidade II");
+//   if (imc >= 40) alert("obesidade III");
+// }
+// console.log(imc);
+
+
+console.time("t"); //start new timer for label name: "t"
+let arr = []; //store empty array
+for(let i = 0; i < 1000000; i++) { //1 million iterations
+arr.push(i); //push current i value
+}
+console.timeEnd("t"); //stop the timer for label name: "t" and print elapsed time
