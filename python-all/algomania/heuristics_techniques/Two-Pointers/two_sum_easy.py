6ff2aa51f4c116619a3fd0a5b81242a53ad17aca
kalinux <kalinux@kalinux.kalinux.com>
2023-10-17 22:56:47 -0400

    Reactjs with vite for quick recap plus a couple of python things too

--- python-all/algomania/heuristics_techniques/Two-Pointers/two_sum_easy.py
+++ python-all/algomania/heuristics_techniques/Two-Pointers/two_sum_easy.py
@@ -16,7 +16,41 @@
     -2 + 11 = 9
 """
 
-# Two Sum - My Solution
+# Two Sum
+
+# My solution attempt:
+  
+def solution(numbers, target_sum):
+    numbers = [4, 1, 2, -2, 11, 15, 1, -1, -6, -4]
+    num_iter = iter(numbers)
+    target_sum = [9]
+    for i,j in range(len(numbers)):
+        numbers[i].next(num_iter)
+        numbers[j].next(num_iter)
+        if i + numbers[j] == target_sum:
+            return []
+        else:
+             print('Try another way!!!')
+             
+# My solution attempt 2:
+   
+def solution(numbers, target_sum):
+    numbers = [4, 1, 2, -2, 11, 15, 1, -1, -6, -4]
+    num_iter = iter(numbers)
+    target_sum = [9]
+    for i in range(len(numbers)):
+        numbers2 = next(num_iter)
+        if i + numbers2 == target_sum:
+            return [i, numbers2]
+        print('Try another way 4!!!')
+
+# Their Solution
 
 def solution(numbers, target_sum):
-    pass
+	for number1_index in range(len(numbers)):
+		number1 = numbers[number1_index]
+		   
+		for number2_index in range(number1_index + 1, len(numbers)):
+			if number1 + numbers[number2_index] == target_sum:
+				return [number1, numbers[number2_index]]
+	return []
